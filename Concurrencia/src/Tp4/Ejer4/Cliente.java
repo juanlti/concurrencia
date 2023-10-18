8348b050f7a3e5a5b02797657f7fbd8390b3dc4b
Juan Cruz <47389717+juanlti@users.noreply.github.com>
2023-10-18 20:13:46 -0300

    Update Cliente.java

--- Concurrencia/src/Tp4/Ejer4/Cliente.java
+++ Concurrencia/src/Tp4/Ejer4/Cliente.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Tp4.Ejer4;
+package Tp4.ejer4;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,26 +13,28 @@ import java.util.logging.Logger;
  * @author juanc
  */
 public class Cliente extends Thread {
-    
+
     GestionImpresion unaGestion;
     int unTiempo;
-    public  Cliente(GestionImpresion unaGestion, int unTiempo){
-        this.unaGestion=unaGestion;
-        this.unTiempo=unTiempo;
-    }
-    
-    
-    public void run(){
-    
-        try {
-            //tiempo de cliente para realizar la impresion
-            System.out.println("Tiempo de cliente");
-            Thread.sleep(unTiempo*1,3);
-            this.unaGestion.gestion(this, unTiempo);
 
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    
+    public Cliente(GestionImpresion unaGestion, int unTiempo) {
+        this.unaGestion = unaGestion;
+        this.unTiempo = unTiempo;
+    }
+
+    public void run() {
+
+       
+            try {
+
+                //tiempo de cliente para realizar la impresion
+                System.out.println("Tiempo de cliente :" + unTiempo * 1.3);
+                Thread.sleep(unTiempo * 1, 3);
+                this.unaGestion.gestion(this, unTiempo);
+
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
     }
 }
