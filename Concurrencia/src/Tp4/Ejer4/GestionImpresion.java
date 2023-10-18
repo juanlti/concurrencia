2ef3a24b7e5b87dd3c413187b9b37244700c433c
Juan Cruz <47389717+juanlti@users.noreply.github.com>
2023-10-18 20:13:00 -0300

    Update GestionImpresion.java

--- Concurrencia/src/Tp4/Ejer4/GestionImpresion.java
+++ Concurrencia/src/Tp4/Ejer4/GestionImpresion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Tp4.Ejer4;
+package Tp4.ejer4;
 
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
@@ -19,6 +19,7 @@ public class GestionImpresion {
     private boolean[] impresorasDisponibles;
     private Semaphore impresoras;
     private int cantImpresorasDisponibles;
+    private Semaphore mutex1, mutex2;
 
     public GestionImpresion(Impresora[] unaImpresa) {
         this.cantImpresorasDisponibles = unaImpresa.length;
@@ -38,46 +39,58 @@ public class GestionImpresion {
 
         }
 
+        mutex1 = new Semaphore(1);
+        mutex2 = new Semaphore(1);
+
     }
 
     public void gestion(Thread unCliente, int unTiempo) {
 
-        if (this.impresoras.availablePermits() > 0) {
+        try {
+            
+            //Adquiero la solicitud (un documento),
+            // si hay una impresora, imprimi documento
+            // no hay disponibilidad, no imprimi documento y sale
+            //  impresoras.acquire(this.cantImpresorasDisponibles);
+            mutex1.acquire(); //MUTEX P 0
+            
+            
+            
+            //al menos una impresora esta disponible
+            int idImpresoraSelecionada = -1;
 
-            try {
-                //Adquiero la solicitud (un documento),
-                // si hay una impresora, imprimi documento
-                // no hay disponibilidad, no imprimi documento y sale
-                impresoras.acquire(1);
+            for (int i = 0; i < todasLasImPresoras.length; i++) {
+                           //  mutex1.acquire(0);  //MUTEX P 1
 
-                //al menos una impresora esta disponible
-                int idImpresoraSelecionada = -1;
+                if (impresorasDisponibles[i]) {
+                    //impresora disponible encontrada
+                    idImpresoraSelecionada = i;
+                    impresorasDisponibles[i] = false;
+                    mutex1.release(); //MUTEX P 1
+                    break; // salgo del bucle
+                } 
+                    
+                
 
-                for (int i = 0; i < todasLasImPresoras.length; i++) {
-                    if (impresorasDisponibles[i]) {
-                        //impresora disponible encontrada
-                        idImpresoraSelecionada = i;
-                        impresorasDisponibles[i] = false;
-                        break; // salgo del bucle
-                    }
-
-                }
-                if (idImpresoraSelecionada != -1) {
-                    // realiza impresion
-                    imprimiendo(idImpresoraSelecionada, unCliente, unTiempo);
-                    //libero impresora
-                    impresorasDisponibles[idImpresoraSelecionada] = true;
-                    System.out.println("Libero impresora "+idImpresoraSelecionada);
-                }
-                //libero impresora, cliente ya termino
-                impresoras.release(1);
-
-            } catch (InterruptedException ex) {
-                System.out.println("Cliente interrumpido " + ex);
             }
 
-        }else{ System.out.println(unCliente.getName()+" sin impresora ");}
-        
+            if (idImpresoraSelecionada != -1) {
+              
+                // realiza impresion
+                imprimiendo(idImpresoraSelecionada, unCliente, unTiempo);
+                //libero impresora
+                //mutex.aq()
+                mutex2.acquire(); //MUTEX2 P 0
+                impresorasDisponibles[idImpresoraSelecionada] = true;
+                //mutex.release()
+                System.out.println("Libero impresora " + idImpresoraSelecionada);
+                mutex2.release(); //MUTEX2 P 1
+            }
+
+        } catch (InterruptedException ex) {
+            System.out.println("Cliente interrumpido " + ex);
+        }
+
     }
 
     public void imprimiendo(int idImpresora, Thread unCliente, int unTiempo) {
