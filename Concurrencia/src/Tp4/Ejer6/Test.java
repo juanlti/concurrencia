6cd98994fd7093e845d1b4e5ff4a55443a3cf65c
Juan Cruz <47389717+juanlti@users.noreply.github.com>
2023-10-26 21:17:49 -0300

    Add files via upload

---
+++ Concurrencia/src/Tp4/Ejer6/Test.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tp4.ejer6;
+
+/**
+ *
+ * @author juan.barrera
+ */
+public class Test {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Taxi unTaxi = new Taxi(250);
+        Pasajero[] unPasajero = new Pasajero[10];
+        Conductor unConductor = new Conductor(unTaxi);
+
+        for (int i = 0; i < unPasajero.length; i++) {
+            unPasajero[i] = new Pasajero(unTaxi, i);
+        }
+        unConductor.start();
+
+        for (int i = 0; i < unPasajero.length; i++) {
+            unPasajero[i].start();
+        }
+
+    }
+
+}
